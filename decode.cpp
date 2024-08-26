5ee107f641057928bd1f5fa0ed878b42380614ab
Mirka1405 <96072040+Mirka1405@users.noreply.github.com>
2024-08-26 19:33:43 +0300

    Update decode.cpp

--- decode.cpp
+++ decode.cpp
@@ -1,12 +1,10 @@
 #include <fstream>
-#include <iostream>
 int main(int argc, char** argv){
     std::ifstream in(argv[1], std::ios::binary);
     std::ofstream out(argv[2], std::ios::binary);
     char nontpcounter = 0;
     while(true){
         unsigned char i = in.get();
-        std::cout<<(int)i<<" ";
         if(in.eof()) break;
         if(nontpcounter>0){out<<i;nontpcounter--;continue;}
         if(i>0xaf) {nontpcounter=i-0xaf;continue;}
